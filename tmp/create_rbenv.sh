495c5b1f0dcf375c51d85d94f3280d977fda38a2
yasuhide sakai <pico_yasu_99_50@yahoo.co.jp>
2017-05-30 23:59:11 +0900

    一般ユーザ用にした

--- tmp/create_rbenv.sh
+++ tmp/create_rbenv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo yum install -y git gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel bzip2
-git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
-git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-echo'export RBENV_ROOT="/usr/local/rbenv"' >> /etc/profile
-echo'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile
-echo'eval "$(rbenv init -)"' >> /etc/profile
-source /etc/profile
+echo'export RBENV_ROOT="~/rbenv"' >> ~/.bash_profile
+echo'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> ~/.bash_profile
+echo'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
